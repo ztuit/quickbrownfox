713bdacc8f4391f020ddaa111079ffbe6896b4aa
Stuart Thompson <stuart.mailme@gmail.com>
2018-09-01 20:39:39 +0100

    Issue #4 word removed if buffer filled

--- src/app/basic.store.ts
+++ src/app/basic.store.ts
@@ -23,15 +23,26 @@ export function rootReducer(lastState: IAppState, action: Action): IAppState {
     case QbfActions.ACTION_TIMER_TICK: return lastState;
     case QbfActions.ACTION_CHARACTER_ADDED:
       var actionWP = action as ActionWithPayload;
-      return { currentBuffer: ''+actionWP.payload, words:lastState.words, score: lastState.score };
+      return checkScore({ currentBuffer: ''+actionWP.payload, words:lastState.words, score: lastState.score });
     case QbfActions.ACTION_WORD_ADDED:
       var actionWP = action as ActionWithPayload;
       var wordSet = lastState.words.slice(0)
       wordSet.push({label:''+actionWP.payload, value:1, speed:1});
       return {currentBuffer:lastState.currentBuffer, words:wordSet,score: lastState.score}
-
-
 }
+
+function checkScore(appState : IAppState) : IAppState {
+      var fw = appState.words.find(function(w){
+        return w.label===appState.currentBuffer;
+      });
+
+      if(fw){
+          appState.score += fw.value;
+          var index = appState.words.indexOf(fw);
+          appState.words.splice(index,1);
+      }
+      return appState;
+    }
 
   // We don't care about any other actions right now.
   return lastState;
